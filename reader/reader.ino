da514ae24df70724739b982b2b6b74828f96f9c5
Lev Zelenskiy <lev.zelensky@gmail.com>
2012-06-11 16:38:07 +1000

    Basic noise level reader
    Reads analog sensor input pin and prints value to serial every second.

---
+++ reader/reader.ino
@@ -0,0 +1,13 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int analogValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  analogValue = analogRead(sensorPin);
+  Serial.println(analogValue);
+  delay(1000);
+}
